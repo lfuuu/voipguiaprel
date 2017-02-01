a7dfb45a472167b7f2dd8b1618b98d697b7b523a
Denis Oleynik <nedlosster@gmail.com>
2017-02-01 13:42:15 +0300

    Добавлены два флага-атрибута в auth.trunk - megatrunk_transfer_to_region , megatrunk_transfer_to_megatrunk

--- models/Trunk.php
+++ models/Trunk.php
@@ -40,7 +40,7 @@ class Trunk extends \yii\db\ActiveRecord
             [['name'], 'string', 'max' => 50],
             [['trunk_name','trunk_name_alias'], 'string', 'max' => 32],
             [['default_priority'], 'integer', 'min'=> -10, 'max' => 10],
-            [['auto_routing', 'source_rule_default_allowed', 'destination_rule_default_allowed', 'source_trunk_rule_default_allowed',
+            [['auto_routing', 'source_rule_default_allowed', 'destination_rule_default_allowed', 'source_trunk_rule_default_allowed','megatrunk_transfer_to_region','megatrunk_transfer_to_megatrunk',
                 'our_trunk','auth_by_number','orig_redirect_number_7800','orig_redirect_number','term_redirect_number','show_in_stat','sw_minimalki','sw_shared'], 'boolean'],
             [['route_table_id','capacity','load_warning','road_to_region'], 'integer'],
         ];
