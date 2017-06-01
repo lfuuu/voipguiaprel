f48850657cae425a8b21d667dbf56102621d31e5
Igor Oferkin <oferkin@mcn.ru>
2017-06-01 11:36:26 +0300

    refactored rows of 'STAGE' type displaying for Тестирование маршрутизации

--- web/js/controllers/TestAuthShowTestPrimary.js
+++ web/js/controllers/TestAuthShowTestPrimary.js
@@ -6,6 +6,9 @@ var TestAuthShowTestPrimaryCtrl = function($scope, TestAuth, params, $modalInsta
         TestAuth.result({id: params.id}).then(function (data) {
             $scope.item = data.item;
             $scope.result = data.result;
+            $scope.row_type_is_stage = function (row) {
+                return row.type == 'STAGE';
+            };
         });
     } else {
         $scope.item = {
