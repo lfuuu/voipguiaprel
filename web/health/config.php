c22eac9b026b69e935b8b329117c407f517cce8f
Roman Elistratov <romane@mcn.ru>
2017-06-05 16:40:18 +0300

    HealthMonitor. Remove database resource, fix display alot of errors

---
+++ web/health/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'resources' => [
+        'reg10.mcntelecom.ru',
+        'reg11.mcntelecom.ru',
+        'reg12.mcntelecom.ru',
+        'reg13.mcntelecom.ru',
+        'reg14.mcntelecom.ru',
+        'reg81.mcntelecom.ru',
+        'reg87.mcntelecom.ru',
+        'reg88.mcntelecom.ru',
+        'reg89.mcntelecom.ru',
+        'reg94.mcntelecom.ru',
+        'reg98.mcntelecom.ru',
+        'reg99.mcntelecom.ru',
+    ],
+];
