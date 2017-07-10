46e18cecab853f7808929cbbb6fee2346e2eab8e
Roman Elistratov <romane@mcn.ru>
2017-07-10 14:47:22 +0300

    HealthMonitorVpbx. Append VPBX monitor, fix resource link

--- web/health/health.php
+++ web/health/health.php
@@ -33,7 +33,9 @@ foreach ($config['resources'] as $resource) {
     $dataJSON = json_decode($data, $assoc = true);
 
     if (json_last_error() === JSON_ERROR_NONE) {
-        $result[$resourceData['host']] = $resource;
+        $result[$resourceData['host']] = array_merge($dataJSON, [
+            'resourceUrl' => $resource,
+        ]);
     } else {
         $result[$resourceData['host']] = $data;
     }
