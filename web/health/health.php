58bd5abbed8a2352e295aa7e433817c76e7e8674
Roman Elistratov <romane@mcn.ru>
2017-07-20 13:53:32 +0300

    HealthMonitor. Fix non object data error, append overflow at info block, make config.local

--- web/health/health.php
+++ web/health/health.php
@@ -1,6 +1,6 @@
 <?php
 
-define('CONFIG_FILEPATH', __DIR__ . '/config.local.php');
+define('CONFIG_FILEPATH', __DIR__ . '/config.php');
 define('RESULT_FILEPATH', __DIR__ . '/../assets/healthData.json');
 
 try {
