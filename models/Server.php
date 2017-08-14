4e72b6ed2cda8ad8b8cebe788afc7ef03d074a39
merinovkv <merinov@mcn.ru>
2017-08-14 13:50:41 +0300

    Исправлена ошибка "Getting unknown property: app\models\Server::instance_settings_id"

--- models/Server.php
+++ models/Server.php
@@ -64,7 +64,7 @@ class Server extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['low_balance_outcome_id', 'blocked_outcome_id', 'hub_id', 'emergency_prefixlist_id', 'mcn_prefixlist_id','rc_mgmn_outcome_id', 'instance_settings_id'], 'integer'],
+            [['low_balance_outcome_id', 'blocked_outcome_id', 'hub_id', 'emergency_prefixlist_id', 'mcn_prefixlist_id','rc_mgmn_outcome_id'], 'integer'],
             [['is_sormed', ], 'boolean'],
             [['calling_station_id_for_line_without_number'], 'string', 'max' => 100],
             [   [
