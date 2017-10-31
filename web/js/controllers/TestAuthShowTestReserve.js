77d15cafe743030685c615449a8844236364a234
svirin-vadim <svirin-vadim@users.noreply.github.com>
2017-10-31 14:36:08 +0300

    Правки для тест резерва (#141)

--- web/js/controllers/TestAuthShowTestReserve.js
+++ web/js/controllers/TestAuthShowTestReserve.js
@@ -9,6 +9,10 @@ var TestAuthShowTestReserveCtrl = function($scope, TestAuth, params, $modalInsta
             $scope.isStageRowType = function (row) {
                 return row.type == 'STAGE';
             };
+            $scope.result_new = data.result_new;
+            if ($scope.result_new == null) {
+                $scope.details = 4;
+            }
         });
     } else {
         $scope.item = {
