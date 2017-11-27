0c456f5877aa0448dc897847359036d05cd6504e
Денис Олейник <nedlosster@MBP-Denis.Dlink>
2017-11-27 08:25:22 +0300

    Уточнение скрипта;

--- install/docker/voip-gui/build.sh
+++ install/docker/voip-gui/build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-
 echo "Собираем образ voip-gui"
 
-[ ! -f "conf/db-local.php" ] &&  echo "Ошибка: Настройте Ваш conf/db-local.php" ; exit 1
-[ ! -f "conf/log-local.php" ] &&  echo "Ошибка: Настройте Ваш conf/log-local.php" ; exit 1
-[ ! -f "conf/params-local.php" ] &&  echo "Ошибка: Настройте Ваш conf/params-local.php" ; exit 1
+[ ! -f "./conf/db-local.php" ] && ( echo "Ошибка: Настройте Ваш conf/db-local.php" ; exit 1 )
+[ ! -f "./conf/log-local.php" ] && ( echo "Ошибка: Настройте Ваш conf/log-local.php" ; exit 1 )
+[ ! -f "./conf/params-local.php" ] && ( echo "Ошибка: Настройте Ваш conf/params-local.php" ; exit 1 )
 
 cp ~/.ssh/id_rsa tmp/id_rsa
 docker build --rm -t voip_gui .
