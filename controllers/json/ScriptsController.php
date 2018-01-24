c3bcc226d46f1cf4e9d68230b5782a651556a72f
Lev Melnikov <lew.ka@hotmail.com>
2018-01-24 14:48:49 +0300

    Добавление команды python в начало команды (#224)

--- controllers/json/ScriptsController.php
+++ controllers/json/ScriptsController.php
@@ -13,7 +13,7 @@ class ScriptsController extends JsonController
             throw new ForbiddenHttpException('Access denied');
         }
     
-        $command = './../scripts/generate_autotest/generate_autotest.py 2>&1 -c ../../generate_autotest.conf > ../scripts/generate_autotest/log.txt';
+        $command = 'python ./../scripts/generate_autotest/generate_autotest.py 2>&1 -c ../../generate_autotest.conf > ../scripts/generate_autotest/log.txt';
         
         shell_exec($command);
         
@@ -26,7 +26,7 @@ class ScriptsController extends JsonController
             throw new ForbiddenHttpException('Access denied');
         }
     
-        $command = './../scripts/generate_autotest/generate_autotest.py 2>&1 -c ../../generate_autotest.conf -delete > ../scripts/generate_autotest/log.txt';
+        $command = 'python ./../scripts/generate_autotest/generate_autotest.py 2>&1 -c ../../generate_autotest.conf -delete > ../scripts/generate_autotest/log.txt';
     
         shell_exec($command);
     
