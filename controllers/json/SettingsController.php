1d1ce7d06404ec3d0fe42fb8aac72f3213065d1c
svirin-vadim <svirin-vadim@users.noreply.github.com>
2018-05-14 10:44:35 +0300

    Добавлено поле в настройки сервера (#290)

--- controllers/json/SettingsController.php
+++ controllers/json/SettingsController.php
@@ -55,7 +55,8 @@ class SettingsController extends JsonController
             'is_route_to_class5' => $server->is_route_to_class5,
             'is_route_to_class5_phase1_enable' => $server->is_route_to_class5_phase1_enable,
             'number_id_filter_b_route_to_class5' => $server->number_id_filter_b_route_to_class5,
-            'cpc_id' => $server->cpc_id
+            'cpc_id' => $server->cpc_id,
+            'is_autotest_error_enabled' => $server->is_autotest_error_enabled
         ];
     }
 
