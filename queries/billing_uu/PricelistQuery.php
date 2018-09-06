e688dfe7375118ebf5b21ab035dac0c682cafe2d
svirin-vadim <svirin-vadim@users.noreply.github.com>
2018-09-06 12:38:56 +0300

    Забытый файл (#333)

---
+++ queries/billing_uu/PricelistQuery.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\queries\billing_uu;
+
+use yii\db\ActiveQuery;
+use app\models\billing_uu\Pricelist;
+
+/**
+ * @method Pricelist[] all($db = null)
+ * @property
+ */
+class PricelistQuery extends ActiveQuery
+{
+}
