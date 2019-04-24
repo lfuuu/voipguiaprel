cdbe24e0b050b559f5ec0569682301b4757e431f
svirin-vadim <svirin-vadim@users.noreply.github.com>
2019-04-24 13:07:19 +0300

    Переключение на более старую версию либы ui-bootstrap (#555)

--- assets/AppLibAsset.php
+++ assets/AppLibAsset.php
@@ -20,7 +20,7 @@ class AppLibAsset extends AssetBundle
         'lib/angularjs/angular.min.js',
 //        'lib/angularjs/i18n/angular-locale_ru-ru.js',
         'lib/angularjs/angular-cookies.min.js',
-        'lib/ui-bootstrap-tpls-0.14.3.min.js',
+        'lib/ui-bootstrap-tpls-0.10.0.min.js',
         'lib/select2/select2.js',
         'lib/ui-select2/select2.js',
         'lib/ui-sortable/sortable.js',
