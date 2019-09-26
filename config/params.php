7a20162dcb67b3fea90b1a3232101954bb82b064
svirin-vadim <svirin-vadim@users.noreply.github.com>
2019-09-26 09:29:53 +0300

    Увеличена максимальная длина комментария (#612)

--- config/params.php
+++ config/params.php
@@ -5,7 +5,7 @@ return [
     'NnpCalculationApi' => '',
     'prefixListTypeSevenOpenLink' => 'http://reg10.mcntelecom.ru:8032/test/nnpcalc?cmd=showTypeSeven',
     'prefixListTypeSevenGenerateLink' => 'http://reg10.mcntelecom.ru:8032/test/nnpcalc?cmd=fillNNPPrefixList&type={type}&id={id}&ndc_token={token}',
-    'commentMaxLength' => 250,
+    'commentMaxLength' => 512,
     'loggingEnabled' => true,
     'logReadMethods' => true
 ];
