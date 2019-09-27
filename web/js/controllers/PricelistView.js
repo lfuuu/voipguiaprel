e5bce3d4be4c374798c4c121e7e913aa5c69319e
svirin-vadim <svirin-vadim@users.noreply.github.com>
2019-09-27 16:44:39 +0300

    Фикс бага v2 (#620)

--- web/js/controllers/PricelistView.js
+++ web/js/controllers/PricelistView.js
@@ -113,7 +113,7 @@ var PricelistViewCtrl = function ($scope, Redirect, List, Pricelist, PricelistLo
                     nnp_region: (item.nnp_region == '{}' && item.f_inv_nnp_region) ? 'Запрещено все!' : (item.f_inv_nnp_region ? ('Кроме: ' + item.nnp_region_name) : item.nnp_region_name),
                     nnp_city: (item.nnp_city == '{}' && item.f_inv_nnp_city) ? 'Запрещено все!' : (item.f_inv_nnp_city ? ('Кроме: ' + item.nnp_city_name) : item.nnp_city_name),
                     nnp_ndc_type: (item.nnp_ndc_type == '{}' && item.f_inv_nnp_ndc_type) ? 'Запрещено все!' : (item.f_inv_nnp_ndc_type ? ('Кроме: ' + item.nnp_ndc_type_name) : item.nnp_ndc_type_name),
-                    nnp_ndc: item.nnp_ndc.replace('{', '').replace('}', '').replace(',', ', '),
+                    nnp_ndc: (item.nnp_ndc) ? item.nnp_ndc.replace('{', '').replace('}', '').replace(',', ', ') : '',
                     rn_replacement_probability: item.rn_replacement_probability,
                     time_start: item.time_start,
                     time_end: item.time_end,
