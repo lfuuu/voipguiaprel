f3bf1838e6c644be77e7ea0e6fe96cf858f3f06c
svirin-vadim <svirin-vadim@users.noreply.github.com>
2019-11-08 16:29:28 +0300

    Расширение комментария до 2048 символов (#660)

--- config/params.php
+++ config/params.php
@@ -5,7 +5,7 @@ return [
     'NnpCalculationApi' => '',
     'prefixListTypeSevenOpenLink' => 'http://reg10.mcntelecom.ru:8032/test/nnpcalc?cmd=showTypeSeven',
     'prefixListTypeSevenGenerateLink' => 'http://reg10.mcntelecom.ru:8032/test/nnpcalc?cmd=fillNNPPrefixList&type={type}&id={id}&ndc_token={token}',
-    'commentMaxLength' => 512,
+    'commentMaxLength' => 2048,
     'loggingEnabled' => true,
     'logReadMethods' => true
 ];
