07a58394432c723a5c4ca00f9222c4de42dfacbd
AlexPerov <shaokahn.ap@gmail.com>
2020-06-29 14:12:04 +0300

    minikube_00_prepare ожидание доступа к реестру (#756)
    * minikube_00_prepare ожидание доступа к реестру
    
    * Update minikube-00-prepare.sh
    
    Co-authored-by: alexander_perov <perov@mcn.ru>
    Co-authored-by: Denis Oleynik <nedlosster@gmail.com>

--- k8s/minikube-00-prepare.sh
+++ k8s/minikube-00-prepare.sh
@@ -1,8 +1,8 @@
 # Этот скрипт нужно запустить в отдельном терминале.
 # Он подготавливает minikube. Запускает в нем службу реестра и ингресса. а так же выставляет службу реестра наружу.
 
-minikube start --addons registry --addons ingress
-
+minikube stop
+minikube start --addon registry --addon ingress
 kubectl -n kube-system expose rc/registry --type=ClusterIP --port=5000 --target-port=5000 --name=werf-registry
 
 export REGISTRY_IP=$(kubectl -n kube-system get svc/werf-registry -o=template={{.spec.clusterIP}})
@@ -12,4 +12,7 @@ minikube ssh "echo '$REGISTRY_IP werf-registry.kube-system.svc.cluster.local' | 
 sudo sed -i -e '/^.*werf-registry.*l$/d' /etc/hosts
 echo "127.0.0.1 werf-registry.kube-system.svc.cluster.local" | sudo tee -a /etc/hosts
 
+
+kubectl wait --for=condition=available --timeout=60s --all deployments -A
+kubectl -n kube-system wait --for=condition=ready pods -l actual-registry=true
 kubectl port-forward --namespace kube-system service/werf-registry 5000
