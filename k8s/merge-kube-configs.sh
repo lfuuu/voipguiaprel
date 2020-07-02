1c2dfdeee823dc6bd372c8bc3c7dbd6b5d0fa9d0
AlexPerov <shaokahn.ap@gmail.com>
2020-07-02 16:37:54 +0300

    merge kubeconfig (#763)
    Co-authored-by: alexander_perov <perov@mcn.ru>

---
+++ k8s/merge-kube-configs.sh
@@ -0,0 +1,7 @@
+cp $HOME/.kube/config $HOME/.kube/config_save
+configs=$HOME/.kube/config
+for file in ~/.kube/*config*
+do
+	[ ! "$file" = "$HOME/.kube/config" ] && configs="$configs:$file"
+done
+KUBECONFIG=$configs kubectl config view --merge --flatten > ~/.kube/config
