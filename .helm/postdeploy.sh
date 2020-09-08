a65ed9b2449b19199091e756168b8fde74a23ad3
AlexPerov <shaokahn.ap@gmail.com>
2020-09-08 16:35:54 +0300

    fix postdeploy (#792)
    Co-authored-by: alexander_perov <perov@mcn.ru>

--- .helm/postdeploy.sh
+++ .helm/postdeploy.sh
@@ -2,3 +2,12 @@
 
 rm .werf/tmp/id_rsa
 rm .werf/tmp/.pgpass
+
+if [ $ENVNAME = "dev" ]; then
+    echo "Копируем ключ ssh"
+    PODNAME="$APPNAME-backend-dev-0"
+    NAMESPACE="$APPNAME-$ENVNAME"
+    kubectl -n $NAMESPACE wait --for=condition=ready --timeout=120s pods $PODNAME
+    kubectl -n $NAMESPACE exec -it $PODNAME -- /bin/bash /root/prepare-scripts/copy_id_rsa.sh `base64 -w 0 ~/.ssh/id_rsa`
+    kubectl -n $NAMESPACE exec -it $PODNAME -- /bin/bash /root/prepare-scripts/init-dev-env.sh
+fi
