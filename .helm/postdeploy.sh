e8530d12118a2349bfb701ae3bad27243fae87a5
Denis Oleynik <nedlosster@gmail.com>
2021-02-03 15:01:16 +0300

    fix

--- .helm/postdeploy.sh
+++ .helm/postdeploy.sh
@@ -4,15 +4,16 @@ THIS=`readlink -f "${BASH_SOURCE[0]}"`
 DIR=`dirname "${THIS}"`
 cd $DIR
 
-rm -rf .werf/tmp/id_rsa
-rm -rf .werf/tmp/.pgpass
-
 source ./def.sh
 
 env=$1
 $env
 
 if [ "$ENVNAME" = "dev" ]; then
+
+    rm -rf .werf/tmp/id_rsa
+    rm -rf .werf/tmp/.pgpass
+
     echo "-- Подготовка workspace"
     PODNAME="$APPNAME-backend-dev-0"
     NAMESPACE="$APPNAME-$ENVNAME"
--- .helm/predeploy.sh
+++ .helm/predeploy.sh
@@ -2,7 +2,12 @@
 
 THIS=`readlink -f "${BASH_SOURCE[0]}"`
 DIR=`dirname "${THIS}"`
-cd $DIR
 
+
+if [ "$ENVNAME" = "dev" ]; then
+
+cd $DIR
 cp ~/.ssh/id_rsa .werf/tmp/id_rsa
 cp ~/.pgpass .werf/tmp/.pgpass
+
+fi
