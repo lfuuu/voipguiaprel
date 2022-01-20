a65a4ea147a8590ceda8b66fa11e535f93ee372f
itar3 <62651854+itar3@users.noreply.github.com>
2022-01-20 14:31:59 +0300

    Billing API: pricelist fix (#937)

--- controllers/json/api_billing/ApiPricelistController.php
+++ controllers/json/api_billing/ApiPricelistController.php
@@ -3,6 +3,7 @@
 namespace app\controllers\json\api_billing;
 
 use app\classes\JsonController;
+use app\models\billing_api\ApiPricelist;
 use app\models\billing_api\ApiPricelistItem;
 
 class ApiPricelistController extends JsonController
@@ -16,16 +17,84 @@ class ApiPricelistController extends JsonController
     protected $editPermission = 'api_billing_api_pricelist_edit';
     protected $deletePermission = 'api_billing_api_pricelist_delete';
 
-    protected function performBeforeSaveActions($item, $request)
+    public function actionSave()
     {
-        ApiPricelistItem::deleteByPricelist($item);
-        if (isset($this->request['items'])) {
-            foreach ($this->request['items'] as $itemData) {
-                $subitem = ApiPricelistItem::create($itemData, $item);
-                if (!$subitem->save()) {
-                    throw new FormValidationException($subitem);
+        if (!\Yii::$app->user->can($this->editPermission) && !\Yii::$app->user->can($this->createPermission)) {
+            throw new ForbiddenHttpException('Access denied');
+        }
+
+        $modelName = $this->modelName;
+
+        $result = [];
+
+        if (isset($this->request[$this->idParamName])) {
+            if (!\Yii::$app->user->can($this->editPermission)) {
+                throw new ForbiddenHttpException('Access denied');
+            }
+
+            $item = $modelName::findOne($this->request[$this->idParamName]);
+
+            if ($item === null) {
+                if ($this->throwExceptionOnEmptyItemInSave) {
+                    throw new HttpException(404, $this->modelName . ' не найден');
+                } else {
+                    if (!\Yii::$app->user->can($this->createPermission)) {
+                        throw new ForbiddenHttpException('Access denied');
+                    }
+
+                    $item = $modelName::create();
+                    $result['log'] = ['data_before' => []];
+                }
+            } else {
+                $result['log'] = ['data_before' => self::getDataForLog($item)];
+            }
+
+        } else {
+            if (!\Yii::$app->user->can($this->createPermission)) {
+                throw new ForbiddenHttpException('Access denied');
+            }
+
+            $item = $modelName::create();
+            $result['log'] = ['data_before' => []];
+        }
+
+        $item->load($this->request, '');
+
+        $this->performBeforeSaveActions($item, $this->request);
+
+        $transaction = $modelName::getDb()->beginTransaction();
+        try {
+            if (!$item->save()) {
+                throw new FormValidationException($item);
+            }
+
+            if (isset($this->request['items'])) {
+                foreach ($this->request['items'] as $apiItem) {
+                    if (!$currentApiItem = ApiPricelistItem::findOne(['id' => $apiItem['id']])) {
+                        $currentApiItem = ApiPricelistItem::create($apiItem, $item);
+                    } else {
+                        foreach ($apiItem as $field => $value) {
+                            $currentApiItem->$field = $value;
+                        }
+                    }
+
+                    if (!$currentApiItem->save()) {
+                        throw new FormValidationException($currentApiItem);
+                    }
+        
                 }
             }
+
+            $this->performAfterSaveActions($item, $this->request);
+
+            $transaction->commit();
+        } finally {
+            if ($transaction->getIsActive())
+                $transaction->rollBack();
         }
+
+        $result['log']['data_after'] = self::getDataForLog($item);
+
+        return $result;
     }
 }
