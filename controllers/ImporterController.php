095f494134f01838ec9e4958d7a364006c53dcc1
Daniil Oleynik <70819792+lfuuu@users.noreply.github.com>
2024-05-28 12:52:46 +0300

    fix (#1065)

--- controllers/ImporterController.php
+++ controllers/ImporterController.php
@@ -24,69 +24,82 @@ class ImporterController extends BaseController
         ini_set('memory_limit', '-1');
         ini_set('max_execution_time', 0);
 
-        $startTime = microtime(true);
-        $prefixesToSave = Yii::$app->cache->get($key);
-        $historyObjectList = Yii::$app->cache->get($key . '_history');
-        $maxDateStart = date('Y-m-d');
+        $transaction = Yii::$app->db->beginTransaction();
 
-        $historyItems = [];
-        $oldItemsIdsByDate = [];
+        try {
+            $startTime = microtime(true);
+            $prefixesToSave = Yii::$app->cache->get($key);
+            $historyObjectList = Yii::$app->cache->get($key . '_history');
+            $maxDateStart = date('Y-m-d');
 
-        foreach ($prefixesToSave as $prefixDateStart => $prefixesToSaveList) {
-            if ($maxDateStart < $prefixDateStart) {
-                $maxDateStart = $prefixDateStart;
+            $historyItems = [];
+            $oldItemsIdsByDate = [];
+
+            foreach ($prefixesToSave as $prefixDateStart => $prefixesToSaveList) {
+                if ($maxDateStart < $prefixDateStart) {
+                    $maxDateStart = $prefixDateStart;
+                }
+
+                $oldItems = $this->getOldItems($id, $prefixDateStart);
+                $oldItemsKeyValue = $this->groupOldItemsByPrefix($oldItems);
+
+                $historyObject = $historyObjectList[$prefixDateStart];
+                $historyItemsIds[] = $historyObject->id;
+
+                $historyObject->date_to = '3000-01-01';
+                $historyObject->fillDataBefore();
+                $oldItemsHistory[$historyObject->id] = [];
+
+                list($finalPrefixesList, $skippedPrefixesList, $oldItemsIds, $historyItems) = $this->processPrefixesToSave($prefixesToSaveList, $oldItemsKeyValue, $historyObject->id);
+
+                $historyObject->total_count = count($finalPrefixesList);
+                $historyObject->save();
+
+                $this->updateOldItemsDateTo($oldItemsIds, $prefixDateStart, $historyObject->id);
+                $this->updateSkippedItemsHistory($skippedPrefixesList, $historyObject->id);
+                $this->batchInsertNewItems($finalPrefixesList);
+
+                if (!empty($oldItemsIds)) {
+                    $oldItemsIdsByDate[] = $oldItemsIds;
+                }
             }
 
-            $oldItems = $this->getOldItems($id, $prefixDateStart);
-            $oldItemsKeyValue = $this->groupOldItemsByPrefix($oldItems);
-
-            $historyObject = $historyObjectList[$prefixDateStart];
-            $historyItemsIds[] = $historyObject->id;
-
-            $historyObject->date_to = '3000-01-01';
-            $historyObject->fillDataBefore();
-            $oldItemsHistory[$historyObject->id] = [];
-
-            list($finalPrefixesList, $skippedPrefixesList, $oldItemsIds, $historyItems) = $this->processPrefixesToSave($prefixesToSaveList, $oldItemsKeyValue, $historyObject->id);
-
-            $historyObject->total_count = count($finalPrefixesList);
-            $historyObject->save();
-
-            $this->updateOldItemsDateTo($oldItemsIds, $prefixDateStart, $historyObject->id);
-            $this->updateSkippedItemsHistory($skippedPrefixesList, $historyObject->id);
-            $this->batchInsertNewItems($finalPrefixesList);
-
-            // Сохраняем oldItemsIds по датам
-            if (!empty($oldItemsIds)) {
-                $oldItemsIdsByDate[] = $oldItemsIds;
+            if ($is_replace) {
+                $this->handleReplaceMode($id, $maxDateStart, $historyItemsIds, $oldItemsIdsByDate);
             }
+
+            \Yii::$app->db->createCommand()->batchInsert(
+                'billing_uu.pricelist_prefix_price_history_item',
+                ['pricelist_prefix_price_history_id', 'prefix_b', 'price_old', 'price_new', 'date_from', 'date_to', 'type'],
+                $historyItems
+            )->execute();
+
+            $transaction->commit();
+
+            $endTime = microtime(true);
+
+            return $this->render('import', [
+                'delta_time' => round($endTime - $startTime, 2)
+            ]);
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
         }
-
-        if ($is_replace) {
-            $this->handleReplaceMode($id, $maxDateStart, $historyItemsIds, $oldItemsIdsByDate);
-        }
-
-        \Yii::$app->db->createCommand()->batchInsert(
-            'billing_uu.pricelist_prefix_price_history_item',
-            ['pricelist_prefix_price_history_id', 'prefix_b', 'price_old', 'price_new', 'date_from', 'date_to', 'type'],
-            $historyItems
-        )->execute();
-
-        $endTime = microtime(true);
-
-        return $this->render('import', [
-            'delta_time' => round($endTime - $startTime, 2)
-        ]);
     }
 
     private function getOldItems($id, $prefixDateStart)
     {
-        return PricelistPrefixPrice::find()
-            ->select(['id', 'prefix_b', 'b_number_price', 'date_to'])
-            ->where(['pricelist_filter_b_id' => $id])
-            ->andWhere('date_to > :date_to', [':date_to' => $prefixDateStart])
-            ->asArray()
-            ->all();
+        $sql = "
+            SELECT id, prefix_b, b_number_price, date_to
+            FROM billing_uu.pricelist_prefix_price
+            WHERE pricelist_filter_b_id = :id AND date_to > :date_to
+            FOR UPDATE
+        ";
+        
+        return Yii::$app->db->createCommand($sql)
+            ->bindValue(':id', $id)
+            ->bindValue(':date_to', $prefixDateStart)
+            ->queryAll();
     }
 
     private function groupOldItemsByPrefix($oldItems)
