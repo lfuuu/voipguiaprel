5d2d9db345bdd97bfc4fab17073a3a0c9e2ca38f
Daniil Oleynik <lfuuu@yandex.ru>
2025-03-06 14:57:30 +0000

    add globalsettings

--- web/js/controllers/settings/GlobalSettings.js
+++ web/js/controllers/settings/GlobalSettings.js
@@ -1,43 +1,32 @@
 var GlobalSettingsCtrl = function($rootScope, $scope, GlobalSettings, $modalInstance) {
     GlobalSettings.get().then(function(data) {
         $scope.settings = data;
+        $scope.originalSettings = angular.copy(data);
     });
-
-    $scope.onAntifraudErrorChange = function() {
-        if ($scope.settings.antifraud_error_check) {
-            GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_error_accept' })
-                .then(function(response) {
-                });
-        } else {
-
-            GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_error_manual' })
-                .then(function(response) {
-                });
-        }
-    };
-
-    $scope.onAntifraudRejectChange = function() {
-        if ($scope.settings.antifraud_reject_check) {
-            GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_reject_accept' })
-                .then(function(response) {});
-        } else {
-            GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_reject_manual' })
-                .then(function(response) {});
-        }
-    };
-
-    $scope.onAntifraudTimeoutChange = function() {
-        if ($scope.settings.antifraud_timeout_check) {
-            GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_timeout_accept' })
-                .then(function(response) {});
-        } else {
-            GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_timeout_manual' })
-                .then(function(response) {});
-        }
-    };
 
     $scope.save = function() {
         GlobalSettings.update($scope.settings).then(function(response) {
+            if ($scope.originalSettings.antifraud_error_check !== $scope.settings.antifraud_error_check) {
+                if ($scope.settings.antifraud_error_check) {
+                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_error_accept' });
+                } else {
+                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_error_manual' });
+                }
+            }
+            if ($scope.originalSettings.antifraud_reject_check !== $scope.settings.antifraud_reject_check) {
+                if ($scope.settings.antifraud_reject_check) {
+                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_reject_accept' });
+                } else {
+                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_reject_manual' });
+                }
+            }
+            if ($scope.originalSettings.antifraud_timeout_check !== $scope.settings.antifraud_timeout_check) {
+                if ($scope.settings.antifraud_timeout_check) {
+                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_timeout_accept' });
+                } else {
+                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_timeout_manual' });
+                }
+            }
             $modalInstance.close();
         });
     };
