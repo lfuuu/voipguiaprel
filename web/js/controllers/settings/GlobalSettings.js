ad5068878dc6130d54c59e85089658713f972692
Daniil Oleynik <lfuuu@yandex.ru>
2025-04-21 12:28:28 +0000

    add call_detail

--- web/js/controllers/settings/GlobalSettings.js
+++ web/js/controllers/settings/GlobalSettings.js
@@ -1,37 +1,47 @@
-var GlobalSettingsCtrl = function($rootScope, $scope, GlobalSettings, $modalInstance) {
+var GlobalSettingsCtrl = function($scope, GlobalSettings, $modalInstance) {
     GlobalSettings.get().then(function(data) {
-        $scope.settings = data;
-        $scope.originalSettings = angular.copy(data);
+      $scope.settings         = data;
+      $scope.originalSettings = angular.copy(data);
     });
-
+  
+    $scope.setMode = function(type, accept) {
+      if (type === 'error') {
+        $scope.settings.antifraud_error_check = accept;
+      }
+      if (type === 'reject') {
+        $scope.settings.antifraud_reject_check = accept;
+      }
+      if (type === 'timeout') {
+        $scope.settings.antifraud_timeout_check = accept;
+      }
+    };
+  
     $scope.save = function() {
-        GlobalSettings.update($scope.settings).then(function(response) {
-            if ($scope.originalSettings.antifraud_error_check !== $scope.settings.antifraud_error_check) {
-                if ($scope.settings.antifraud_error_check) {
-                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_error_accept' });
-                } else {
-                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_error_manual' });
-                }
-            }
-            if ($scope.originalSettings.antifraud_reject_check !== $scope.settings.antifraud_reject_check) {
-                if ($scope.settings.antifraud_reject_check) {
-                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_reject_accept' });
-                } else {
-                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_reject_manual' });
-                }
-            }
-            if ($scope.originalSettings.antifraud_timeout_check !== $scope.settings.antifraud_timeout_check) {
-                if ($scope.settings.antifraud_timeout_check) {
-                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_timeout_accept' });
-                } else {
-                    GlobalSettings.callProcedure({ procedure: 'public.set_antifraud_timeout_manual' });
-                }
-            }
-            $modalInstance.close();
+        const toCall = [
+          $scope.settings.antifraud_error_check
+            ? 'public.set_antifraud_error_accept'
+            : 'public.set_antifraud_error_manual',
+          $scope.settings.antifraud_reject_check
+            ? 'public.set_antifraud_reject_accept'
+            : 'public.set_antifraud_reject_manual',
+          $scope.settings.antifraud_timeout_check
+            ? 'public.set_antifraud_timeout_accept'
+            : 'public.set_antifraud_timeout_manual'
+        ];
+      
+        let seq = Promise.resolve();
+        toCall.forEach(proc => {
+          seq = seq.then(() => GlobalSettings.callProcedure({ procedure: proc }));
         });
+      
+        seq.then(() => {
+          $modalInstance.close();
+        });
+      };
+      
+  
+    $scope.close = function() {
+      $modalInstance.close();
     };
-
-    $scope.cancel = function() {
-        $modalInstance.dismiss();
-    };
-};
+  };
+  
