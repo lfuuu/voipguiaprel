8c4b83fc3f0320486fcbda470871d1cc25a611ef
Daniil Oleynik <lfuuu@yandex.ru>
2025-05-13 13:50:19 +0000

    573-2 fix

--- controllers/json/calls_detail/ReportCallController.php
+++ controllers/json/calls_detail/ReportCallController.php
@@ -5,7 +5,9 @@ namespace app\controllers\json\calls_detail;
 use app\classes\JsonController;
 use app\models\calls_detail\ReportCall;
 use yii\web\HttpException;
+use yii\web\Response;
 use yii\db\Expression;
+use yii\db\Query;
 
 class ReportCallController extends JsonController
 {
@@ -14,6 +16,7 @@ class ReportCallController extends JsonController
      */
     public function actionIndex()
     {
+        \Yii::$app->response->format = Response::FORMAT_JSON;
         $mcn = \Yii::$app->request->get('mcn_callid');
         if (!$mcn) {
             throw new HttpException(400, 'Не указан mcn_callid');
@@ -24,13 +27,31 @@ class ReportCallController extends JsonController
                 'call_type_id',
                 'csv_formated',
                 new Expression("
-                  concat(
-                    'Оригинационный Транк: ', incoming_trunk_debug,
-                    '; Терминационный Транк: ', outgoing_trunk_debug,
-                    '; Нода: ', switch_name_id_debug
-                  ) AS trunks_info
+                    concat(
+                      'Оригинационный Транк: ', incoming_trunk_debug,
+                      '; Терминационный Транк: ', outgoing_trunk_debug,
+                      '; Нода: ', switch_name_id_debug
+                    ) AS trunks_info
                 ")
             ])
+            ->where(['mcn_callid' => $mcn])
+            ->asArray()
+            ->all();
+    }
+
+    /**
+     * GET /json/calls-detail/report-call/debug?mcn_callid=...
+     * Debug-режим: возвращает ВСЕ поля из таблицы report_call
+     */
+    public function actionDebug()
+    {
+        \Yii::$app->response->format = Response::FORMAT_JSON;
+        $mcn = \Yii::$app->request->get('mcn_callid');
+        if (!$mcn) {
+            throw new HttpException(400, 'Не указан mcn_callid');
+        }
+
+        return ReportCall::find()
             ->where(['mcn_callid' => $mcn])
             ->asArray()
             ->all();
