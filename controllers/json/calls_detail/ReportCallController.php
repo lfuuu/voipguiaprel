bc7e82fbdac1a765a077c0f6868dda2bb677a97d
Daniil Oleynik <lfuuu@yandex.ru>
2025-05-21 15:29:32 +0000

    fix 573-2

--- controllers/json/calls_detail/ReportCallController.php
+++ controllers/json/calls_detail/ReportCallController.php
@@ -15,29 +15,31 @@ class ReportCallController extends JsonController
      * GET /json/calls-detail/report-call?mcn_callid=...
      */
     public function actionIndex()
-    {
-        \Yii::$app->response->format = Response::FORMAT_JSON;
-        $mcn = \Yii::$app->request->get('mcn_callid');
-        if (!$mcn) {
-            throw new HttpException(400, 'Не указан mcn_callid');
-        }
-    
-        $isDebug = (bool)\Yii::$app->request->get('is_debug');
-        $query = ReportCall::find()->where(['mcn_callid' => $mcn]);
-    
-        if ($isDebug) {
-            return $query->asArray()->all();
-        }
-    
-        $results = $query
-            ->select(['csv_formated'])
-            ->asArray()
-            ->all();
-    
-        return array_map(function($row) {
-            return rtrim($row['csv_formated'], "\r\n");
-        }, $results);
+{
+    $mcn = \Yii::$app->request->get('mcn_callid');
+    if (!$mcn) {
+        throw new HttpException(400, 'Не указан mcn_callid');
     }
+
+    $isDebug = (bool)\Yii::$app->request->get('is_debug');
+    $query = ReportCall::find()->where(['mcn_callid' => $mcn]);
+
+    if ($isDebug) {
+        \Yii::$app->response->format = Response::FORMAT_JSON;
+        return $query->asArray()->all();
+    }
+
+    $results = $query
+        ->select(['csv_formated'])
+        ->asArray()
+        ->all();
+
+    \Yii::$app->response->format = Response::FORMAT_RAW;
+    return implode("\n", array_map(function($row) {
+        return rtrim($row['csv_formated'], "\r\n");
+    }, $results));
+}
+
     
 
 }
