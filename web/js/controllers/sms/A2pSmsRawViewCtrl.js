0ce6cfe2da8b84d9b4a12a318a7f2a5de1eff247
Daniil Oleynik <lfuuu@yandex.ru>
2025-05-26 14:25:29 +0000

    add a2p sms raw

--- web/js/controllers/sms/A2pSmsRawViewCtrl.js
+++ web/js/controllers/sms/A2pSmsRawViewCtrl.js
@@ -1,20 +1,23 @@
-var A2pSmsRawViewCtrl = function($scope, $modalInstance, $http, params, $sce) {
-    // Из параметров получаем cdr_id, для которого ищем A2P SMS данные
-    $scope.cdrId = params.cdr_id;
-    $scope.a2pSmsDetails = [];
-    $scope.errorMessage = null;
+var A2pSmsRawViewCtrl = function($scope, $modalInstance, $http, params) {
+  // Получаем cdr_id для поиска raw A2P-SMS
+  $scope.cdrId       = params.cdr_id;
+  $scope.smsDetails  = [];
+  $scope.errorMessage = null;
+  $scope.loading     = true;
 
-    // Выполняем GET-запрос по URL, соответствующему маршруту контроллера
-    $http.get('/json/a2p_sms/a2p-sms-raw/raw', { params: { cdr_id: $scope.cdrId } })
-         .then(function(response) {
-             $scope.a2pSmsDetails = response.data;
-         })
-         .catch(function(error) {
-             $scope.errorMessage = "Ошибка: " + (error.data.error || error.statusText);
-         });
-    
-    // Функция закрытия модального окна
-    $scope.back = function() {
-        $modalInstance.dismiss();
-    };
+  // Запрос к нашему новому контроллеру
+  $http.get('/json/sms/a2p-sms-raw/raw', {
+    params: { cdr_id: $scope.cdrId }
+  }).then(function(response) {
+    $scope.smsDetails = response.data;
+    $scope.loading    = false;
+  }).catch(function(error) {
+    $scope.errorMessage = 'Ошибка: ' + (error.data.error || error.statusText);
+    $scope.loading      = false;
+  });
+
+  // Закрыть модалку
+  $scope.close = function() {
+    $modalInstance.dismiss();
+  };
 };
