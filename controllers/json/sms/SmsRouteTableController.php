f29a1e5f523b73539b06ea9a9e2199716585c02a
Daniil Oleynik <lfuuu@yandex.ru>
2025-06-02 15:25:02 +0000

    fix history on route table

--- controllers/json/sms/SmsRouteTableController.php
+++ controllers/json/sms/SmsRouteTableController.php
@@ -4,31 +4,94 @@ namespace app\controllers\json\sms;
 
 use app\classes\JsonController;
 use app\exceptions\FormValidationException;
+use app\models\auth\SmsRouteTable;
 use app\models\auth\SmsRouteTableRoute;
+use yii\web\ForbiddenHttpException;
+use yii\db\Transaction;
 
 class SmsRouteTableController extends JsonController
 {
-    protected $modelName = 'app\models\auth\SmsRouteTable';
-    protected $idParamName = 'id';
-    protected $nameParamName = 'name';
+    protected $modelName        = 'app\models\auth\SmsRouteTable';
+    protected $idParamName      = 'id';
+    protected $nameParamName    = 'name';
     protected $withDependencies = ['routes'];
-    protected $readWhere = ['server_id'];
+    protected $readWhere        = ['server_id'];
     protected $createPermission = 'sms_route_table_create';
-    protected $listPermission = 'sms_route_table_list';
-    protected $editPermission = 'sms_route_table_edit';
+    protected $listPermission   = 'sms_route_table_list';
+    protected $editPermission   = 'sms_route_table_edit';
     protected $deletePermission = 'sms_route_table_delete';
 
-    protected function performAfterSaveActions($item, $request)
+    protected function getDataForLog($item)
     {
-        SmsRouteTableRoute::deleteByRouteTable($item);
-        $order = 1;
-        foreach ($this->request['routes'] as $routeData) {
-            $route = SmsRouteTableRoute::create($item, $routeData);
-            $route->order = $order;
-            if (!$route->save()) {
-                throw new FormValidationException($route);
-            }
-            $order++;
+        $data = $item->toArray();
+        $data['routes'] = [];
+        foreach ($item->routes as $route) {
+            $data['routes'][] = $route->toArray();
         }
+        return $data;
+    }
+
+    public function actionSave()
+    {
+        if (!\Yii::$app->user->can(
+             isset($this->request['id'])
+               ? $this->editPermission
+               : $this->createPermission
+           )) {
+            throw new ForbiddenHttpException('Access denied');
+        }
+
+        $isNew = empty($this->request['id']);
+        if ($isNew) {
+            $item = SmsRouteTable::create();
+        } else {
+            $item = SmsRouteTable::findOne($this->request['id']);
+            if (!$item) {
+                throw new \yii\web\HttpException(404, 'SmsRouteTable not found');
+            }
+        }
+
+        $item->load($this->request, '');
+
+        $transaction = SmsRouteTable::getDb()->beginTransaction(Transaction::SERIALIZABLE);
+        try {
+            $dataBefore = $this->getDataForLog($item);
+
+            if (!$item->save()) {
+                throw new FormValidationException($item);
+            }
+
+            SmsRouteTableRoute::deleteByRouteTable($item);
+            $order = 1;
+            foreach ($this->request['routes'] as $routeData) {
+                $route = SmsRouteTableRoute::create($item, $routeData);
+                $route->order = $order++;
+                if (!$route->save()) {
+                    throw new FormValidationException($route);
+                }
+            }
+
+            $transaction->commit();
+        } catch (\Exception $e) {
+            if ($transaction->isActive) {
+                $transaction->rollBack();
+            }
+            throw $e;
+        }
+
+        $item->refresh();              
+        $item = SmsRouteTable::find()
+            ->with('routes')
+            ->where(['id' => $item->id])
+            ->one();
+
+        $dataAfter = $this->getDataForLog($item);
+
+        return [
+            'log' => [
+                'data_before' => $dataBefore,
+                'data_after'  => $dataAfter,
+            ]
+        ];
     }
 }
--- web/js/controllers/sms/SmsRouteTableEdit.js
+++ web/js/controllers/sms/SmsRouteTableEdit.js
@@ -1,69 +1,83 @@
-var SmsRouteTableEditCtrl = function($rootScope, $scope, Redirect, SmsRouteTable, SmsList, params, $modalInstance, Number) {
-    if (params.id) {
-        SmsRouteTable.get({id: params.id}).then(function (data) {
-            console.log(data);
-            $scope.item = data;
-        });
-    } else {
-        $scope.item = {
-            name: '',
-            server_id: $scope.server.id,
-            routes: []
-        };
-    }
-
-    $scope.sortableOptions = {
-        update: function (e, ui) {
-            var sortBlocked = false;
-
-            if (ui.item.sortable.index < ui.item.sortable.dropindex) {
-                var dropMin = ui.item.sortable.index;
-                var dropMax = ui.item.sortable.dropindex;
-            } else {
-                var dropMin = ui.item.sortable.dropindex;
-                var dropMax = ui.item.sortable.index;
-            }
-
-            for (var routeKey in $scope.item.routes) {
-                if (routeKey >= dropMin && routeKey <= dropMax && $scope.item.routes[routeKey]['is_locked']) {
-                    sortBlocked = true;
-                }
-            }
-
-            if (sortBlocked) {
-                ui.item.sortable.cancel();
-            }
-        },
-        axis: 'y'
+var SmsRouteTableEditCtrl = function(
+  $rootScope,
+  $scope,
+  Redirect,
+  SmsRouteTable,
+  SmsList,
+  params,
+  $modalInstance,
+  Number
+) {
+  if (params.id) {
+    SmsRouteTable.get({ id: params.id }).then(function(data) {
+      console.log(data);
+      $scope.item = data;
+    });
+  } else {
+    $scope.item = {
+      name: '',
+      server_id: $scope.server.id,
+      routes: []
     };
+  }
 
-    $scope.addRoute = function () {
-        $scope.item.routes.push({a_number_id: null});
-    };
+  $scope.sortableOptions = {
+    update: function(e, ui) {
+      var sortBlocked = false;
 
-    $scope.removeRoute = function (index) {
-        $scope.item.routes.splice(index, 1);
-    };
+      if (ui.item.sortable.index < ui.item.sortable.dropindex) {
+        var dropMin = ui.item.sortable.index;
+        var dropMax = ui.item.sortable.dropindex;
+      } else {
+        var dropMin = ui.item.sortable.dropindex;
+        var dropMax = ui.item.sortable.index;
+      }
 
-    $scope.viewActionLog = function() {
+      for (var routeKey in $scope.item.routes) {
+        if (
+          routeKey >= dropMin &&
+          routeKey <= dropMax &&
+          $scope.item.routes[routeKey]['is_locked']
+        ) {
+          sortBlocked = true;
+        }
+      }
+
+      if (sortBlocked) {
+        ui.item.sortable.cancel();
+      }
+    },
+    axis: 'y'
+  };
+
+  $scope.addRoute = function() {
+    $scope.item.routes.push({ a_number_id: null });
+  };
+
+  $scope.removeRoute = function(index) {
+    $scope.item.routes.splice(index, 1);
+  };
+
+  $scope.viewActionLog = function() {
     if (!$scope.item.id) return;
     Redirect.actionLogView(
       $scope.item.id,
       'json/sms/sms-route-table',
       'save'
     ).then(function() {
+      // ничего не делаем
     });
   };
 
-    $scope.save = function () {
-        var data = angular.copy($scope.item);
+  $scope.save = function() {
+    var data = angular.copy($scope.item);
+    SmsRouteTable.save(data).then(function(res) {
+  $modalInstance.close();
+});
 
-        SmsRouteTable.save(data).then(function () {
-            $modalInstance.close();
-        });
-    };
+  };
 
-    $scope.back = function () {
-        $modalInstance.dismiss();
-    };
+  $scope.back = function() {
+    $modalInstance.dismiss();
+  };
 };
