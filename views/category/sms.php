17458f3b9f894458ec51b896fb7df10f417260eb
Daniil Oleynik <lfuuu@yandex.ru>
2025-07-08 15:22:15 +0000

    добавление сущности Узлы передачи данных

--- views/category/sms.php
+++ views/category/sms.php
@@ -1,24 +1,37 @@
 <?php
+use app\commands\RbacController;
+use yii\helpers\Json;
 
-use app\models\ServerOcs;
-use yii\helpers\Url;
+/** @var \app\components\View $this */
+/** @var app\models\ServerOcs[] $servers */
 
-/**
- * @var app\components\View $this
- * @var ServerOcs[] $servers
- */
+$userPerms = Yii::$app->authManager
+    ->getPermissionsByUser(Yii::$app->user->identity->getId());
+$shortUserPerms = [];
+foreach ($userPerms as $k => $p) {
+    $shortUserPerms[$k] = true;
+}
+
+$firstServer = !empty($servers) ? reset($servers) : null;
 ?>
+<script>
+  var userName         = <?= Json::encode(Yii::$app->user->identity->name,  JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) ?>;
+  var userId           = <?= Json::encode(Yii::$app->user->identity->getId(), JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) ?>;
+  var userPermissions  = <?= Json::encode($shortUserPerms,                 JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) ?>;
+  var billingPermissions = <?= Json::encode(RbacController::getBillingPermissions(), JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) ?>;
+  var smsPermissions   = <?= Json::encode(RbacController::getSmsPermissions(),      JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) ?>;
+  var query            = <?= Json::encode($_SERVER['QUERY_STRING'],         JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) ?>;
+  var host             = <?= Json::encode($_SERVER['HTTP_HOST'],           JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) ?>;
 
-<table class="table table-striped table-hover table-condensed">
-    <thead>
-        <tr>
-            <th style="width:20%">Код</th>
-            <th style="width:20%">Название</th>
-        </tr>
-    </thead>
-        <tr>
-            <td style="cursor: pointer" onclick="location.href='<?= Url::toRoute(['sms/index', 'serverId' => $servers->id]); ?>'"><?= $servers->id ?></td>
-            <td style="cursor: pointer" onclick="location.href='<?= Url::toRoute(['sms/index', 'serverId' => $servers->id]); ?>'"><?= $servers->name ?></td>
-        </tr>
-    </tbody>
-</table>
+  <?php if ($firstServer): ?>
+  var dataServer = {
+      id:   <?= Json::encode($firstServer->id,   JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) ?>,
+      name: <?= Json::encode($firstServer->name, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) ?>
+  };
+  <?php endif; ?>
+</script>
+
+<div ng-controller="MainSmsCtrl"
+     ng-init="selectServer(dataServer)"
+     ng-include="'templates/main_sms.html'">
+</div>
