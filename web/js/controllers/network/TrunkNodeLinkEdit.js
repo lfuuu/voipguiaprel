497d8fa3bcdf09d163c2c6b3dda3489dd1fed741
Daniil Oleynik <lfuuu@yandex.ru>
2025-09-22 13:16:53 +0000

    network fix

--- web/js/controllers/network/TrunkNodeLinkEdit.js
+++ web/js/controllers/network/TrunkNodeLinkEdit.js
@@ -1,32 +1,27 @@
 var TrunkNodeLinkEditCtrl = function($rootScope, $scope, Link, Node, params, $modalInstance, $window, TrunkNodeLink) {
-    Node.listNodesForLink().then(function(data) {
-        $scope.nodesList = data;
+  Node.listNodesForLink().then(function(data) { $scope.nodesList = data; });
+
+  if (params.trunk_node_link_id !== null && params.trunk_node_link_id !== undefined && params.trunk_node_link_id !== '') {
+    TrunkNodeLink.get({ id: params.trunk_node_link_id }).then(function(data) {
+      $scope.item = data;
     });
-    if (params.trunk_node_link_id !== null && params.trunk_node_link_id !== undefined && params.trunk_node_link_id !== '') {
-      TrunkNodeLink.get({ id: params.trunk_node_link_id }).then(function(data) {
-        $scope.item = data;
-      });
-      
-    } else {
-      $scope.item = {
-        service_trunk_id: '',
-        node_id: '',
-        comment: '',
-        id_phys_trunk: '',
-        sorm_id: ''
-      };
-    }
-    
-    $scope.save = function() {
-      TrunkNodeLink.save($scope.item).then(function() {
-        $modalInstance.close();
-      });
+  } else {
+    $scope.item = {
+      src_node_id: '',
+      dst_node_id: '',
+      weight: 1,
+      comment: '',
+      sorm_name: 'ООО МСН Телеком',
+      sorm_id: ''
     };
-  
-    $scope.back = function() {
-        $modalInstance.dismiss();
-    };
+  }
+
+  $scope.save = function() {
+    TrunkNodeLink.save($scope.item).then(function() {
+      $modalInstance.close();
+    });
   };
-  
-  app.controller('NodeLinkEditCtrl', NodeLinkEditCtrl);
-  
+
+  $scope.back = function() { $modalInstance.dismiss(); };
+};
+app.controller('TrunkNodeLinkEditCtrl', TrunkNodeLinkEditCtrl);
